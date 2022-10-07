69d83dbc7a5f16efe7d03621de50b4b3489cdab3
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-10-07 17:04:16 +0200

    Add files via upload

---
+++ Evaluación Tema 2/menu.py
@@ -0,0 +1,17 @@
+import os
+import helpers
+import Ej1,Ej2,Ej3,Ej4,Ej5 as Ej1,Ej2,Ej3,Ej4,Ej5
+
+def iniciar():
+    while True:
+        helpers.limpiar_pantalla()
+
+        print("========================")
+        print("  Bienvenido al Menu  ")
+        print("========================")
+        print("[1] Ejecutar Ejercicio 1 ")
+        print("[2] Ejecutar Ejercicio 2 ")
+        print("[3] Ejecutar Ejercicio 3 ")
+        print("[4] Ejecutar Ejercicio 4 ")
+        print("[5] Ejecutar Ejercicio 5 ")
+        print("========================")
